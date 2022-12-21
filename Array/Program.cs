5a56b4405b81b4d928a5ab956d4fa0eca4e76b0b
Gunjan <gunjangoyal1911@gmail.com>
2022-12-21 15:39:03 +0100

    Added Array project

---
+++ Array/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[3];
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[2] { "Gunjan", "Ginni" };
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.ReadLine();
+        }
+    }
+}
